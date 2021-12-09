95caca4df78c5f13d4d43e1f747d3d93d72af826
magcode <marko.web1@outlook.com>
2021-12-09 20:34:42 +0100

    improved logging

--- victoria/polluvic.js
+++ victoria/polluvic.js
@@ -22,7 +22,7 @@ const retryAfter = 10000;
 const retryLimit = 5;
 
 logger.info("PolluComE VictoriaMetrics interface");
-logger.info("Using victoria server: " + vicHost +  ":" + vicPort);
+logger.info("Using victoria server: " + vicHost + ":" + vicPort);
 logger.info("Using binary: " + binary);
 logger.info("Sending data every: " + everyXMinutes + " minute");
 
@@ -31,71 +31,71 @@ const retrier = new Retrier(options);
 
 var timer = "*/" + everyXMinutes + ' * * * *';
 cron.schedule(timer, function () {
-    retrier.resolve(attempt => new Promise(
-        function (resolve, reject) {
+	retrier.resolve(attempt => new Promise(
+		function (resolve, reject) {
 			logger.info("Getting data ...");
 			exec(binary + " -d " + usbport, (error, stdout, stderr) => {
-                if (error) {
-                    logger.error(`error: ${error.message}`);
-                    reject("Error")
-                }
-                if (stderr) {
-                    logger.error(`stderr: ${stderr}`);
-                    reject("Error")
-                }
-                var lines = stdout.toString().split('\n');
+				if (error) {
+					logger.error(`error: ${error.message}`);
+					reject("Error")
+				}
+				if (stderr) {
+					logger.error(`stderr: ${stderr}`);
+					reject("Error")
+				}
+				var lines = stdout.toString().split('\n');
 
-                if (lines.includes("No data received!")) {     
-                    logger.warn("No data received!")
-                    reject("No data")
-                }
-                
-                var data = {};
-                lines.forEach(function (line) {
-                    if (line != '' && !line.includes("No data") && !line.includes("volume") && !line.includes("tempDiff"))  {
-                        var string = line.split(' ');
-                        data[string[0]] = string[1];
-                    }
-                });
-				logger.info("Done.");
+				if (lines.includes("No data received!")) {
+					logger.warn("No data received! Attempt:" + (attempt + 1) + "/" + retryLimit);
+					reject("No data");
+				}
+
+				var data = {};
+				lines.forEach(function (line) {
+					if (line != '' && !line.includes("No data") && !line.includes("volume") && !line.includes("tempDiff")) {
+						var string = line.split(' ');
+						data[string[0]] = string[1];
+					}
+				});
 				resolve(data)
-            });
-        })
-    )
-    .then(
+			});
+		})
+	).then(
 		result => {
-                Object.keys(result).forEach(function (key, index) {
-					mappings.forEach(function(element){
-						if (element.hasOwnProperty(key)) {
-							const data = "home " + element[key] + "=" + result[key]
-							logger.debug("Data: '" + data + "'")
-							const httpOptions = {
-								hostname: vicHost,
-								port: vicPort,
-								path: '/write',
-								method: 'POST',
-								headers: {
-									'Content-Type': 'application/x-www-form-urlencoded',
-									'Content-Length': data.length,
-									'Access-Control-Allow-Origin': '*'
-								}					
+			logger.info("Got data ...");
+			Object.keys(result).forEach(function (key, index) {
+				mappings.forEach(function (element) {
+					if (element.hasOwnProperty(key)) {
+						const data = "home " + element[key] + "=" + result[key]
+						logger.debug("Data: '" + data + "'")
+						const httpOptions = {
+							hostname: vicHost,
+							port: vicPort,
+							path: '/write',
+							method: 'POST',
+							headers: {
+								'Content-Type': 'application/x-www-form-urlencoded',
+								'Content-Length': data.length,
+								'Access-Control-Allow-Origin': '*'
 							}
-							
-							const req = http.request(httpOptions, (res) => {
-									if (res.statusCode!=204) {
-										logger.error(`ERROR, got statusCode from victoria: ${res.statusCode}`)	
-									}
-									
-								})
-							req.write(data)
-							req.end()
-							req.on('error', (error) => {
-								logger.error("ERROR: " + error)
-								})
-						}				
-					});
-                });
+						}
+
+						const req = http.request(httpOptions, (res) => {
+							if (res.statusCode != 204) {
+								logger.error(`ERROR, got statusCode from victoria: ${res.statusCode}`)
+							}
+
+						})
+						req.write(data)
+						req.end()
+						req.on('error', (error) => {
+							logger.error("ERROR: " + error)
+						})
+					}
+				});
+			});
+			logger.info("Data stored.");			
 		},
-        error => logger.error(error)
-    );
+		error => logger.error("No data after " + retryLimit + " attempts")
+	);
 });
